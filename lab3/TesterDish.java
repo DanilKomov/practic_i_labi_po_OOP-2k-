a913d1a267058a09439ecd815f3d99133e31636c
DanilKomov <33160319+DanilKomov@users.noreply.github.com>
2018-10-30 14:15:30 +0300

    Create TesterDish.java

---
+++ lab3/TesterDish.java
@@ -0,0 +1,8 @@
+package ru.mirea.komov.lab3;
+
+public class TesterDish {
+    public static void main(String[] args) {
+        Tarelka t = new Tarelka("Керамическая","Белый","Большая");
+                System.out.println(t);
+    }
+}
