27b03a77bfd5e52e2cd34db2681a5e3aa11bfc5d
DanilKomov <33160319+DanilKomov@users.noreply.github.com>
2018-11-09 14:09:45 +0300

    Create Tester.java

---
+++ Lab4/Tester.java
@@ -0,0 +1,10 @@
+package ru.mirea.komov.lab4;
+
+public class Tester {
+    public static void main(String[] args) {
+        Car c1= new Car("Mercedes-Benz", "S-classe", "S500", 7000000, 2016);
+        c1.println();
+        Car c2 = new Car("BMW", "7 Series", "750 Li", 3500000, 2018);
+        c2.println();
+    }
+}
