7bb1b1cd317cce665e949c901ee79596e7043296
DanilKomov <33160319+DanilKomov@users.noreply.github.com>
2018-12-04 21:32:58 +0300

    Create Main.java

---
+++ prakt1/Main.java
@@ -0,0 +1,8 @@
+package ru.mirea.prakt1;
+
+public class Main {
+    public static void main(String[] args) {
+        Bookb = newBook("Отверженные", "Виктор Гюго", 1800);
+System.out.println(b);
+    }
+}
