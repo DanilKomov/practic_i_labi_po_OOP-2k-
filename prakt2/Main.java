f7d164a1906ceefdc3374f001d802c0ce286577d
DanilKomov <33160319+DanilKomov@users.noreply.github.com>
2018-12-04 21:34:27 +0300

    Create Main.java

---
+++ prakt2/Main.java
@@ -0,0 +1,8 @@
+package ru.mirea.prakt2;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author a = new Author("Bob Chaplin", "anotheronemail@mail.ru", 'm');
+System.out.println(a);
+    }
+}
